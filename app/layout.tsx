b9171e249b16f04c667c6c79b1140746abbe8b7d
karthikram07 <karthikramachandra93@gmail.com>
2024-01-29 11:43:19 +0530

    update config

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,39 +6,39 @@ import { Navbar } from "@/components/Navbar";
 const publicSans = Public_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
-}: {
+                                     children
+                                   }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head>
-        <title>LangChain + Next.js Template</title>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <meta
-          name="description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
-        />
-        <meta property="og:title" content="LangChain + Next.js Template" />
-        <meta
-          property="og:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
-        />
-        <meta property="og:image" content="/images/og-image.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LangChain + Next.js Template" />
-        <meta
-          name="twitter:description"
-          content="Starter template showing how to use LangChain in Next.js projects. See source code and deploy your own at https://github.com/langchain-ai/langchain-nextjs-template!"
-        />
-        <meta name="twitter:image" content="/images/og-image.png" />
-      </head>
-      <body className={publicSans.className}>
-        <div className="flex flex-col p-4 md:p-12 h-[100vh]">
-          <Navbar></Navbar>
-          {children}
-        </div>
-      </body>
+    <head>
+      <title>LangChain + Next.js Template</title>
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <meta
+        name="description"
+        content="NextJS + Langchain retrieval chain"
+      />
+      <meta property="og:title" content="LangChain + Next.js Retrieval Chain" />
+      <meta
+        property="og:description"
+        content="NextJS + Langchain retrieval chain"
+      />
+      <meta property="og:image" content="/images/og-image.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="LangChain + Next.js Template" />
+      <meta
+        name="twitter:description"
+        content="NextJS + Langchain retrieval chain"
+      />
+      <meta name="twitter:image" content="/images/og-image.png" />
+    </head>
+    <body className={publicSans.className}>
+    <div className="flex flex-col p-4 md:p-12 h-[100vh]">
+      <Navbar></Navbar>
+      {children}
+    </div>
+    </body>
     </html>
   );
 }

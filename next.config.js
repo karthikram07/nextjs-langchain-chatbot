6ca54d9f7d794d26438ecb5b23d56ead025ff579
karthikram07 <karthikramachandra93@gmail.com>
2024-01-29 11:22:07 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,11 @@
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-//   future: {
-//
-//     // by default, if you customize webpack config, they switch back to version 4.
-//     // Looks like backward compatibility approach.
-//     webpack5: true
-//   },
-//
-//   webpack(config) {
-//     config.resolve.fallback = {
-//
-//       // if you miss it, all the other options in fallback, specified
-//       // by next.js will be dropped.
-//       ...config.resolve.fallback,
-//
-//       fs: false // the solution
-//     };
-//
-//     return config;
-//   }
-//
-// });
-// module.exports = withBundleAnalyzer({});
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  future: {
 
+    // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+    webpack5: true
+  },
 
   webpack(config) {
     config.resolve.fallback = {
@@ -36,5 +18,15 @@ module.exports = {
     };
 
     return config;
+  },
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true
   }
-};
+
+});
+module.exports = withBundleAnalyzer({});
